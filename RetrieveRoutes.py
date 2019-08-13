6763ffb60a4f78b10698de6927f004d6e9e60b38
Lucas Ritzdorf <lucas.ritzdorf@gmail.com>
2019-08-13 16:23:42 -0600

    Begin fleshing out the route-retrieval program
    Still need to add a method of reading in county and lake data to memory, then getting directions between each
    Consider using a NumPy matrix again?

--- RetrieveRoutes.py
+++ RetrieveRoutes.py
@@ -5,20 +5,38 @@
 
 # Data will be stored in "encoded polyline" form, as a string of characters.
 # This data can be converted to a series of points by the
-# openrouteservice.convert.decode_polyline(enc) function, where enc is the
-# string representing the encoded polyline. This would be done in the program
-# utilizing the stored data.
+# openrouteservice.convert.decode_polyline() function, with the string
+# representing the encoded polyline as its only argument. This would be done in
+# the program utilizing the stored data.
+version = 'v0.1'
 
 
-# Open county file
+import openrouteservice
+
+print(f'OpenRouteService Route Retrieval Program {version}\n')
+
+# Request county, lake, and output files
 countyPath = os.getcwd() + '\\' + input('Type the name of the COUNTY file, ' \
-                                       'relative to your current path:\n')
-countyFile = open(countyPath, 'r')
-# Open lake file
+                                        'relative to your current path:\n')
 lakePath = os.getcwd() + '\\' + input('Type the name of the LAKE file, ' \
-                                       'relative to your current path:\n')
-lakeFile = open(lakePath, 'r')
-# Create and open output file
+                                      'relative to your current path:\n')
+outputPath = os.getcwd() + '\\' + input('\nType the name of the new file to '\
+                                        'be created for OUTPUT, relative to '\
+                                        'your current path:\n')
+# (Try to) Open input files
+(countyFile,lakeFile) = (None,None)
+try:
+    with open(countyPath, 'r') as countyFile, open(lakePath, 'r') as lakeFile:
+        #Internalize county and lake data, ensuring that the most up-to-date
+        #records are used for each lake
+except FileNotFoundError:
+    print(f'\nCould not find {f} file as specified above. Please check for '\
+          'typing errors and try again.\nFull error trace:')
+    raise
+
+
+
+        
 # Query user's ORS API key
 print('\nBe aware that all ORS API requests will be charged against your '\
       'Directions V2 quota in OpenRouteService. The total number of requests '\
@@ -28,10 +46,45 @@ key = input('Type (or paste) your API key here:\n')
 
 # Actual data retrieval
 count = 0
-for cLine in countyFile:
-    for lLine in lakeFile:
-        count += 1
-        #Get route from ORS using queried key
-        #Write route and orig/dest to output file
+client = openrouteservice.Client(key=key)
+with open(outputPath, 'a') as outputFile:
+#    for county in counties:
+#        for lake in lakes:
+            # Get directions for the route and write to output file
+            try:
+#                routes = client.directions((start,end))
+                count += 1
+                encoded = routes['routes'][0]['geometry']
+                #Write route and orig/dest to output file
+            # Address several possible errors returned by the API
+            except openrouteservice.exceptions.ApiError as e:
+                print('An error occurred regarding the ORS Directions query. '\
+                      'A brief description is shown above the full error, as '\
+                      'reported by the server:')
+                if e.args[0] == 401:
+                    print('The API key is missing from the request.')
+                elif e.args[0] == 403:
+                    print('The API key is not valid.')
+                elif e.args[0] == 404:
+                    print('Unable to find requested object')
+                elif e.args[0] == 413:
+                    print('The request is too large.')
+                elif e.args[0] == 500:
+                    print('An unknown server error occurred.')
+                elif e.args[0] == 501:
+                    print('The server cannot fulfill this request.')
+                elif e.args[0] == 503:
+                    print('The server is currently unavailable due to '\
+                          'overload or maintenance.')
+                else:
+                    print('An unlikely error occurred. Please try again. If '\
+                          'the issue persists, something very serious has '\
+                          'changed in the OpenRouteService API.')
+                raise e
+            # And the possibility of a file error
+            except IOError:
+                print('An error occurred while writing data to the output '\
+                      'file. Full error trace:')
+                raise
 
 print(f'Complete; made a total of {count} queries')
